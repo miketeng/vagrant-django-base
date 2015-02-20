b51b050024a5e4f40293c15f7bcc46e18ce8130c
Mike Teng <miketeng@users.noreply.github.com>
2015-02-20 07:19:55 -0800

    add trusty64 to box name to avoid confusion

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # to build django-base-v2.box:
 vagrant up
-rm -f django-base-v2.2.box
-vagrant package --output django-base-v2.2.box
+rm -f django-base-trusty64-v2.2.box
+vagrant package --output django-base-trusty64-v2.2.box
 
 # to install locally:
-# vagrant box add django-base-v2.2 django-base-v2.2.box
+# vagrant box add django-base-v2.2 django-base-trusty64-v2.2.box
